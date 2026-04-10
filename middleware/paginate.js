a939c957528bf0332d4af335406ca570034e9693
Vlad <vlad086367@gmail.com>
2026-04-10 22:37:42 +0300

    Fix paginate

--- middleware/paginate.js
+++ middleware/paginate.js
@@ -2,8 +2,8 @@ module.exports.paginate = (req, res, next) => {
   const { page = 1, result = 5 } = req.query;
 
   req.paginate = {
-    limit: Number(result),
-    offset: (Number(page) - 1) * Number(result),
+    limit: result,
+    offset: (page - 1) * result,
   };
 
   next();
